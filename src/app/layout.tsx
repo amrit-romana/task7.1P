9619950e04c63dff079ec71c029078617d861788
amrit-224236794 <s224236794@deakin.edu.au>
2026-07-08 13:19:20 +1000

    added google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 import { FrontendWrapper } from "@/components/layout/FrontendWrapper";
@@ -30,8 +31,23 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://use.typekit.net" crossOrigin="" />
         <link rel="preconnect" href="https://p.typekit.net" crossOrigin="" />
+        <Script id="gtm-head" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5W48PMB4');`}
+        </Script>
       </head>
       <body suppressHydrationWarning className={`font-futura antialiased selection:bg-stone selection:text-charcoal`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5W48PMB4"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <TypeKitLoader />
         <FrontendWrapper>
           {children}
